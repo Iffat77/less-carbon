39d7e4bbee4771b4b82512432c0bb1311fb64ceb
Iffat Hossain <iffathossain77@gmail.com>
2023-05-03 20:23:39 -0400

    completes routes and controllers for post

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./userRoutes.js";
 import wallRoutes from "./wallRoutes.js";
 import threadRoutes from "./threadRoutes.js";
+import postRoutes from "./postRoutes.js";
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.get("/", (req, res) => res.send("This is the API root!"));
 router.use("/", userRoutes);
 router.use("/", wallRoutes);
 router.use("/", threadRoutes);
+router.use("/", postRoutes);
 
 export default router;
 
