f498ccde6bc145424210409ccd60c2e66b0dd3b3
Iffat Hossain <iffathossain77@gmail.com>
2023-06-06 18:26:17 -0400

    fixes path

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -3,8 +3,8 @@ import api from "./apiConfig";
 
 // Remider to update url before prod NOTE- fugure out api import issue
 
-// const baseUrl = "https://less-carbon-mongodb-api.onrender.com/api/users";
-const baseUrl = "http://localhost:8000/api/users";
+const baseUrl = "https://less-carbon-mongodb-api.onrender.com/api/users";
+// const baseUrl = "http://localhost:8000/api/users";
 
 
 const authService = {
