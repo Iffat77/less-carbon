2971bc62c4735065ec505abd784ec2137f819783
Iffat Hossain <iffathossain77@gmail.com>
2023-06-06 15:17:49 -0400

    re-test deploy

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,25 +1,20 @@
 import axios from "axios";
 
-
 let apiUrl;
 
 const apiUrls = {
   production: "https://less-carbon-mongodb-api.onrender.com/api",
-  // development: "http://localhost:8000/api",
+  development: "http://localhost:8000/api",
 };
 
-apiUrl = apiUrls.production;
+if (window.location.hostname === "localhost") {
+  apiUrl = apiUrls.development;
+} else {
+  apiUrl = apiUrls.production;
+}
 
-// if (window.location.hostname === "localhost") {
-//   apiUrl = apiUrls.development;
-// } else {
-// }
-console.log(apiUrl)
 const api = axios.create({
   baseURL: apiUrl,
 });
 
-
 export default api;
-
-
